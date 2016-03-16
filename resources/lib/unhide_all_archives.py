215f1a09c0f8eb620f395459ed919f3c7db577f1
bbr25 <bbr@manx.net>
2016-03-16 19:52:36 +0000

    Update unhide_all_archives.py

--- resources/lib/unhide_all_archives.py
+++ resources/lib/unhide_all_archives.py
@@ -1,7 +1,7 @@
 import xbmcaddon, xbmcgui
 
-addon = xbmcaddon.Addon(id='plugin.program.iarlsnes')
-current_setting = addon.getSetting('iarlsnes_setting_clear_hidden_archives')
+addon = xbmcaddon.Addon(id='plugin.program.iarlnes')
+current_setting = addon.getSetting('iarlnes_setting_clear_hidden_archives')
 
 if 'false'.lower() in current_setting.lower():
 	current_dialog = xbmcgui.Dialog()
@@ -10,6 +10,6 @@ if 'false'.lower() in current_setting.lower():
 	if ret1 == 0:
 		pass
 	else:
-		addon.setSetting(id='iarlsnes_setting_clear_hidden_archives',value='true')
+		addon.setSetting(id='iarlnes_setting_clear_hidden_archives',value='true')
 		ok_ret = current_dialog.ok('Complete','All Archives will be set to Visible.')
 		print 'IARL:  Unhide All Archives set to true'
